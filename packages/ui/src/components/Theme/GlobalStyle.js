62b4ed50bb83ee1f81625fc9694e159d61332335
Rafael Franco <rafaelfr@outlook.com>
2020-02-28 18:39:08 -0300

    fix: add font-smoothing to GlobalStyle

--- packages/ui/src/components/Theme/GlobalStyle.js
+++ packages/ui/src/components/Theme/GlobalStyle.js
@@ -11,6 +11,8 @@ const GlobalStyle = createGlobalStyle`
     *:before,
     *:after {
       box-sizing: inherit;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
     }
 
     body,
