3eaf3f38036c254fa39b7dc88f5d332c2efe8e12
Rafael Franco <rafaelfr@outlook.com>
2020-02-29 23:22:27 -0300

    fix: export all from xstyled

--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -1,5 +1,3 @@
-import styled from '@xstyled/styled-components'
-
 import { ThemeProvider, theme } from './components/Theme'
 import Box from './components/Box'
 import Button from './components/Button'
@@ -8,6 +6,7 @@ import Heading from './components/Heading'
 import Row from './components/Row'
 import Text from './components/Text'
 
-export { ThemeProvider, theme, Box, Button, Col, Heading, Row, Text }
+export * from '@xstyled/styled-components'
+export { default } from '@xstyled/styled-components'
 
-export default styled
+export { ThemeProvider, theme, Box, Button, Col, Heading, Row, Text }
